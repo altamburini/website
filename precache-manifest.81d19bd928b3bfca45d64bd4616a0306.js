c38b32e6cd63531da0b071e8d46c5dc813c00a25
fefanto <m.nottoli@gmail.com>
2019-10-07 10:13:59 +0200

    Updates

---
+++ precache-manifest.81d19bd928b3bfca45d64bd4616a0306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff62bd18648734b7a5a8aad3dcca642",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4b0f4323328ad348a5be",
+    "url": "/website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "46d6bbd4242576015e0f",
+    "url": "/website/static/js/2.41aa2fe9.chunk.js"
+  },
+  {
+    "revision": "4b0f4323328ad348a5be",
+    "url": "/website/static/js/main.e1b96597.chunk.js"
+  },
+  {
+    "revision": "1c647f033b56b289c0c4",
+    "url": "/website/static/js/runtime-main.a7a2578a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  }
+]);
