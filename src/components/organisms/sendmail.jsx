4a411c78fed96a82729c150968069eb95019e01d
fefanto <m.nottoli@gmail.com>
2019-10-16 16:24:22 +0200

    end of styling

--- src/components/organisms/sendmail.jsx
+++ src/components/organisms/sendmail.jsx
@@ -6,13 +6,14 @@ export default class SendMail extends Component {
       <div>
         <section className="colorlib-blog" data-section="sendmail">
           <div className="colorlib-narrow-content">
-            <form action="mailto:m.nottoli@gmail.com" method="GET" target="_blank">
+            <form action="mailto:alessandro-tamburini@libero.it" method="GET" target="_blank">
               <h1 class="title">Invia un messaggio:</h1>
               <div class="field"><label className="colorlib-heading">Titolo</label>
                 <input name="subject" id="subject" type="text" class="input" value="" /></div>
               <div class="field"><label className="colorlib-heading">Testo</label>
                 <textarea class="textarea" name="body" id="body"></textarea></div>
-              <div><input type="submit" value="Invia" class="button is-primary" /></div>
+              <div>
+                <input type="submit" value="Invia" class="button is-primary" /></div>
             </form>
             <hr />
           </div>
